0e501666590a307c1a37dff44cf87d35c07680ba
Blaze Wheeler <88804767+BlazeWheeler@users.noreply.github.com>
2022-02-13 01:01:25 -0500

    initial commit

---
+++ SwiftChatBot/SwiftChatBot/SwiftChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatBotApp.swift
+//  SwiftChatBot
+//
+//  Created by blaze Wheeler on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
